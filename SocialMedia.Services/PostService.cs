eeb4d1142df48b79e909f682a9061f44de64d4fe
Rob Mongin <RobertMongin@outlook.com>
2021-11-30 20:55:03 -0500

    Post data and models

--- SocialMedia.Services/PostService.cs
+++ SocialMedia.Services/PostService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SocialMedia.Data;
+using SocialMedia.Models.PostModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,74 @@ using System.Threading.Tasks;
 
 namespace SocialMedia.Services
 {
-    class PostService
+    public class PostService
     {
+        private readonly Guid _userId;
+
+        public PostService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        //Create
+        public bool CreatePost(PostCreate model)
+        {
+            var entity = new Post()
+            {
+                OwnerId = _userId,
+                Content = model.Content,
+                CreatedUtc = DateTimeOffset.Now,
+
+            };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Posts.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        //Read
+        public IEnumerable<PostListItem> GetPosts()
+        {
+            using(var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Posts
+                        .Where(e => e.OwnerId == _userId)
+                        .Select(
+                            e =>
+                                new PostListItem
+                                {
+                                    PostId = e.PostId,
+                                    CreatedUtc = e.CreatedUtc
+                                }
+                        );
+                return query.ToArray();
+            }
+        }
+
+        public PostDetail GetPostById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Posts
+                        .Single(e => e.PostId == id && e.OwnerId == _userId);
+                return
+                    new PostDetail
+                    {
+                        PostId = entity.PostId,
+                        Content = entity.Content,
+                        CreatedUtc = entity.CreatedUtc,
+                        ModifiedUtc = entity.ModifiedUtc,
+                    };
+            }
+        }
+
+        //Update
+       
     }
 }
